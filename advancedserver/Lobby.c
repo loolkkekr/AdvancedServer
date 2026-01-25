3b02ba8e44440da226cb8a89095d5dd511f97f50
loolkkekr <98670575+loolkkekr@users.noreply.github.com>
2026-01-25 18:41:59 +0300

    Update Lobby.c

--- advancedserver/Lobby.c
+++ advancedserver/Lobby.c
@@ -8,8 +8,8 @@
 #include <stdbool.h>
 #include <time.h>
 
-#define NO_COUNTDOWN 60
-#define COUNTDOWN 59
+#define NO_COUNTDOWN 61
+#define COUNTDOWN 60
 
 bool lobby_send_countdown(Server* server)
 {
