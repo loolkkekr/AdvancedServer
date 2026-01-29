d89078816f20bc001ae19a1beda126d96dbd79c5
loolkkekr <98670575+loolkkekr@users.noreply.github.com>
2026-01-30 01:55:41 +0300

    гений забыл добавить переменные и сидит тупит, чё у него не компилируется

--- include/States.h
+++ include/States.h
@@ -41,6 +41,8 @@ typedef struct Entity
 #define CMD_VM 59505
 #define CMD_START 1770248296
 #define CMD_REFRESH 3384196449
+#define NO_COUNTDOWN 92
+#define COUNTDOWN 91
 
 bool lobby_init				(Server* server);
 bool lobby_state_join		(PeerData* v);
