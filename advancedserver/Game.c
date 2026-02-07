a8c63b0eb67d8cc54baffa806305957ee65df27d
loolkkekr <98670575+loolkkekr@users.noreply.github.com>
2026-02-08 00:05:58 +0300

    Update Game.c

--- advancedserver/Game.c
+++ advancedserver/Game.c
@@ -1216,7 +1216,7 @@ bool game_state_handletcp(PeerData* v, Packet* packet)
 
 				SET_FLAG(to_revive->plr.flags, PLAYER_REVIVED);
 				DEL_FLAG(to_revive->plr.flags, PLAYER_DEAD);
-				to_revive->plr.heal_rings_collected = 0
+				to_revive->plr.heal_rings_collected = 0;
 				to_revive->plr.expected_hp = 40;
 				PacketCreate(&pack, SERVER_REVIVAL_STATUS);
 				PacketWrite(&pack, packet_write8, 0);
