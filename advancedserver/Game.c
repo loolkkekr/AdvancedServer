6dfea39a05993d3f0bfa599588549cd85bf7038e
loolkkekr <98670575+loolkkekr@users.noreply.github.com>
2026-02-08 00:42:04 +0300

    Update Game.c

--- advancedserver/Game.c
+++ advancedserver/Game.c
@@ -1194,25 +1194,29 @@ bool game_state_handletcp(PeerData* v, Packet* packet)
 					bool has = false;
 					int ind = 0;
 
-					for (int i = 0; i < 5; i++) {
-						if (to_revive->plr.revival_init[i] == -1) break;
-						PeerData* data = server_find_peer(v->server, to_revive->plr.revival_init[i]);
-						if(!data) continue;
-						
-						// Сбрасываем heal_rings_collected у тех, кто помогал с возрождением
-						// (или уменьшаем на количество потраченных колец, если известно)
-						data->plr.heal_rings_collected -= 3; // или -= 3; если тратили 3 кольца
-						
-						PacketCreate(&pack, SERVER_REVIVAL_RINGSUB);
-						packet_send(data->peer, &pack, true);
-						Debug("Removed rings from %d", to_revive->plr.revival_init[i]);
+					for (int i = 0; i < 5; i++)
+					{
+						if (to_revive->plr.revival_init[i] == -1)
+						{
+							ind = i;
+							break;
+						}
+
+						if (to_revive->plr.revival_init[i] == v->id)
+							has = true;
 					}
+
 					if (!has)
 						to_revive->plr.revival_init[ind] = v->id;
 				}
 			}
 			else
 			{
+				for (int i = 0; i < 5; i++)
+				{
+					if (to_revive->plr.revival_init[i] == v->id)
+						data->plr.heal_rings_collected = 0;
+				}
 				to_revive->plr.stats.rings = 0;
 
 				SET_FLAG(to_revive->plr.flags, PLAYER_REVIVED);
